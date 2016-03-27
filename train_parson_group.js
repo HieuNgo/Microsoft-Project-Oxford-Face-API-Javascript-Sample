2620bc8f916bbf3928ec0fb573e0911d28059a48
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2016-03-27 09:29:48 +0900

    some fix

---
+++ train_parson_group.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+var constValues = require('./const/const.js')
+	,request = require('request')
+	,headers = {
+	'Content-Type':'application/json',
+	'Ocp-Apim-Subscription-Key':constValues.APIKEY  
+}
+
+
+
+
+var options = {
+  url: 'https://api.projectoxford.ai/face/v1.0/persongroups/' + constValues.GROUPNAME + '/train',
+  method: 'POST',
+  headers: headers,
+  json:{
+	  "name":"sample-group",
+	  
+  }
+ 
+ 
+}
+
+//send request
+request(options, function (error, response, body) {
+  if(error){
+	  console.log(error);  
+  }
+  console.log(body);
+  
+	
+})　　　
